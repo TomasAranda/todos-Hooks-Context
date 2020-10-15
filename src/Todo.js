7814f1126df9d413bcb913b196a41bf59958c6ed
Tomas Aranda <tomas.aranda.95@gmail.com>
2020-10-15 13:24:31 -0300

    Fix typo on import of MaterialUI Checkbox

--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,7 @@ import useToggleState from './hooks/useToggleState';
 import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import CheckBox from '@material-ui/core/CheckBox';
+import CheckBox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
